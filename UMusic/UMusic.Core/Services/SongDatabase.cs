38faa1fea918b2345b0845aeb43c96cc917d8e99
UGamer <itsugamer@gmail.com>
2019-10-17 21:48:16 -0400

    [PRE-ALPHA] 1.0.0
    Created the app and put resources, also got a basic online function running

---
+++ UMusic/UMusic.Core/Services/SongDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UMusic.Core.Services
+{
+    public class SongDatabase
+    {
+        public static ArrayList GetFiles(string[] paths)
+        {
+            ArrayList files = new ArrayList();
+
+            foreach (string path in paths)
+            {
+                string[] pathFiles = Directory.GetFiles(path);
+
+                foreach (string pathFile in pathFiles)
+                {
+                    files.Add(pathFile);
+                }
+            }
+
+            return files;
+        }
+    }
+}
---
+++ UMusic/UMusic/Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,9 @@
+﻿namespace UMusic.Behaviors
+{
+    public enum NavigationViewHeaderMode
+    {
+        Always,
+        Never,
+        Minimal
+    }
+}
